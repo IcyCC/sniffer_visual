63f7fde4e63706de4b274bc830cd58900ef47d92
suchang <1103232282@qq.com>
2019-01-16 18:11:46 +0800

    完成

--- visual_font/src/service/api/index.js
+++ visual_font/src/service/api/index.js
@@ -1,20 +1,41 @@
 import axios from 'axios'
+
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 
 export const queryRequestInfos = (args) => {
-        return axios.post('/api/request_infos', {
-            _method: 'GET',
-            _args:args
-        })
+    return axios.post('/api/request_infos', {
+        _method: 'GET',
+        _args: args
+    })
 }
 
 export const queryRequestHeaders = (args) => {
     return axios.post('/api/request_headers', {
         _method: 'GET',
-        _args:args
+        _args: args
     })
 }
 
-export const getRquestInfo =(id) => {
+export const getRquestInfo = (id) => {
     return axios.get('/api/request_infos/' + id)
+}
+
+export const queryHostClientRank = () => {
+    return axios.get('/api/queryHostClientRank')
+}
+
+export const queryHostTimetRank = () => {
+    return axios.get('/api/queryHostTimeRank')
+}
+
+export const queryClient = () => {
+    return axios.get('/api/queryClientRank')
+}
+
+export const queryClientSrcLike = (src) =>{
+    return axios.get('/api/client_src', {params: {'_like_src':src}})
+}
+
+export const queryCLientHostRanks = (src) =>{
+    return axios.get('/api/queryClientHostRank/'+src)
 }
