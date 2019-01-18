dd1d727da3ce8e84ea448e445a93bc07bb74506a
Mr-QI <qidongsheng90@gmail.com>
2019-01-18 13:48:39 +0800

    修改数据库

--- sniffer_db/table.sql
+++ sniffer_db/table.sql
@@ -1,18 +1,20 @@
 create table request_headers
 (
- id int auto_increment
-  primary key,
- request_id int not null comment '请求的id',
- header_key varchar(255) default '' not null comment '请求头的值',
- header_value varchar(1024) default '' not null comment '请求头的值'
-);
-
+	id int not null primary key auto_increment comment '本次请求的id',
+	request_id int not null comment '请求id',
+	header_key varchar(255) default '' not null comment '请求头的值',
+	header_value varchar(1024) default '' not null comment '请求头的值'
+)
+;
+create index request_headers_request_id_index on request_headers (request_id);
 create table request_infos
 (
- id int auto_increment
-  primary key,
- src varchar(128) default '' not null,
- dest varchar(128) default '' null,
- created_at timestamp default CURRENT_TIMESTAMP not null,
- type tinyint default '0' not null comment '类型 0 为请求 1为响应'
-);
+	id int not null primary key auto_increment comment '本次请求的id',
+	type tinyint default '0' not null comment '类型 0 为请求 1为响应',
+	src varchar(128) default '' not null comment '本次请求的源地址',
+	dest varchar(128) default '' null comment '本次请求的目的地址',
+	created_at timestamp default CURRENT_TIMESTAMP not null comment '入库时间',
+	host varchar(1024) default '' not null comment '请求的url'
+)
+;
+create index request_infos_src_index on request_infos (src);
